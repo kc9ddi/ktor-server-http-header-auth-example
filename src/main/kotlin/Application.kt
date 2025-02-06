fe56b91744f2f22baedacfd0a8a737178a20b312
Patrick McDonnell <kc9ddi@gmail.com>
2025-02-05 19:05:13 -0600

    initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package cv.mcdonnell
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureRouting()
+}
---
+++ src/main/kotlin/Security.kt
@@ -0,0 +1,8 @@
+package cv.mcdonnell
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureSecurity() {
+}
