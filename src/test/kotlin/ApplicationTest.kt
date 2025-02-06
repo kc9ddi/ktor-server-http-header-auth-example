944aa56907bd679650f00f8597061e56860fc102
Patrick McDonnell <kc9ddi@gmail.com>
2025-02-05 19:33:16 -0600

    Add application tests

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,6 +1,7 @@
 package cv.mcdonnell
 
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
@@ -13,8 +14,23 @@ class ApplicationTest {
         application {
             module()
         }
-        client.get("/").apply {
+        client.get("/unauthenticated").apply {
             assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+
+        client.get("/authenticated").apply {
+            assertEquals(HttpStatusCode.Unauthorized, status)
+        }
+
+        client.get("/authenticated") {
+            headers {
+                append("X-AppEngine-QueueName", "QueueName")
+                append("X-AppEngine-TaskName", "TaskName")
+            }
+        }.apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Successfully authenticated with queueName=QueueName and taskName=TaskName", bodyAsText())
         }
     }
 
